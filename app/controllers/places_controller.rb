6a1ca52783026d1ee6748a8f670a5f80aa9c1331
Christy Burkhalter <christykburkhalter@gmail.com>
2019-08-26 20:23:28 +0000

    error message in edit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,9 +10,13 @@ def new
 end
 
 def create
-  current_user.places.create(place_params)
-  redirect_to root_path
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
   def show
     @place = Place.find(params[:id])
@@ -28,11 +32,17 @@ def create
 
   def update
     @place = Place.find(params[:id])
+
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end 
+
     @place.update_attributes(place_params)
+    if @place.valid?
     redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
